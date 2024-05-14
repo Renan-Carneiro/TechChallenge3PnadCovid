f437179688f740cdef1fc54380d768e218e03d58
Renan Carneiro <113647888+Renan-Carneiro@users.noreply.github.com>
2024-05-13 21:23:11 -0300

    4.4

--- pages/2_Desafio.py
+++ pages/2_Desafio.py
@@ -1,19 +1,20 @@
 import streamlit as st
 from PIL import Image
 
-st.markdown("""**Introdução**
+st.markdown("""
+#**Introdução**
 
 Fomos desafiados a nos imaginarmos como experts em Data Analytics de um grande hospital para entender como foi o comportamento da população durante a pandemia de Coronavírus e para identificar quais seriam os melhores indicadores para o planejamento em um novo cenário de surto da doença.
 
 O estudo do PNAD-COVID-19, realizado pelo IBGE (Instituo Brasileiro de Geografia e Estatística), é uma ótima fonte de dados, contendo todos os dados necessários para enfrentarmos o desafio proposto, além, é claro, de serem confiáveis. Tudo aquilo que é necessário para apresentarmos as análises propostas podem ser encontrados no estudo.
 
-A proposta exigiu que fizéssemos as análises dos seguintes dados:
+##A proposta exigiu que fizéssemos as análises dos seguintes dados:
 
 - Características clínicas dos sintomas;
 - Características da população;
 - Características da sociedade;
 
-Além disso, ao entrarmos na base de dados do PNAD-COVID-19 e organizarmos esta base para análise, trouxemos as seguintes características:
+##Além disso, ao entrarmos na base de dados do PNAD-COVID-19 e organizarmos esta base para análise, trouxemos as seguintes características:
 
 - Utilização de no máximo 20 perguntas feitas na pesquisa;
 - Utilizar 3 meses para construção da solução;
