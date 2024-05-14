55083af354d9ed769f8bd513cef18c1db3c6c763
Renan Carneiro <113647888+Renan-Carneiro@users.noreply.github.com>
2024-05-13 21:30:44 -0300

    Update 3_Banco de Dados.py

--- pages/3_Banco de Dados.py
+++ pages/3_Banco de Dados.py
@@ -7,15 +7,19 @@ st.markdown("""
 # PNAD COVID
 A Pesquisa Nacional por Amostra de Domicílios – COVID-19 ou simplesmente PNAD Covid, não é apenas uma pesquisa, mas uma iniciativa do IBGE (Instituto Brasileiro de Geografia e Estatística) que buscou entender efeitos da pandemia em nosso país.
 Através do PNAD, buscou-se mensurar o impacto da pandemia nas mais diferentes esferas, e, por essa razão, foi necessário um vasto questionário com perguntas envolvendo questões clínicas, socioeconômicas e características da população.
+
 Sem uma pesquisa dessa magnitude, seria extremamente difícil entender os problemas causados, bem como identificar os mais afetados pela maior crise sanitária do século.
+
 Através da análise dos dados colhidos, é possível traçar estratégias políticas que poderão não só minimizar o impacto como também evitar futuros cenários como o que vimos durante a pandemia.
 
 ## Selecionado as perguntas
 Ao selecionar as perguntas para análise dos dados, consideramos a importância de questões que identificam sintomas, impacto na saúde, acesso a serviços médicos e medidas preventivas durante a pandemia. Além disso, incluímos dados sociodemográficos para uma análise mais detalhada. 
 Iniciamos com a abordagem dos sintomas, destacando a importância de questões que identificam sintomas característicos, como febre, perda de cheiro ou sabor, tosse, entre outros, os quais são cruciais para a identificação de possíveis padrões de manifestações clínicas.
+
 Além disso, priorizamos perguntas relacionadas ao impacto na saúde e procura por atendimento, que nos permitem avaliar a gravidade da condição de saúde dos indivíduos, a necessidade de recursos médicos e o acesso aos serviços de saúde, evidenciado também pela questão sobre plano de saúde médico.
 É crucial destacar a complexidade da entubação, procedimento delicado que demanda treinamento específico. A escassez de profissionais qualificados para realizar entubações seguras ressalta a necessidade de investimento na formação e preparo de equipes de saúde.
 Considerando o contexto da pandemia do Coronavírus, incorporamos questionamentos sobre as medidas preventivas adotadas, como restrições de contato social e a presença de itens básicos de limpeza e proteção nos domicílios, essenciais para compreender as práticas preventivas e seu impacto na propagação da doença.
+
 Por fim, a inclusão de dados sociodemográficos, como idade, sexo, etnia, escolaridade, unidade da federação, entre outros, busca fornecer subsídios para uma análise mais detalhada e estratificada dos resultados, considerando as especificidades de diferentes grupos populacionais.
 Dessa forma, a escolha das perguntas para análise reflete a preocupação em obter dados abrangentes e significativos, capazes de contribuir para uma melhor compreensão dos fenômenos em estudo e para o desenvolvimento de estratégias eficazes no âmbito da saúde pública.
 """)
