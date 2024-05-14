15db2f06c482bb62afd35366f94e463d20a1bf60
Renan Carneiro <113647888+Renan-Carneiro@users.noreply.github.com>
2024-05-13 23:04:03 -0300

    graficos

--- pages/3_Banco de Dados.py
+++ pages/3_Banco de Dados.py
@@ -93,6 +93,7 @@ with tab3:
     st.image(image)
     st.markdown("""
     3. Foi utilizado então o google collab para fazer o acesso a essa base que criamos com novas consultas SQL. O notebook gerado está incluso no [github](https://github.com/Renan-Carneiro/TechChallenge3PnadCovid).
+    
     Exemplo de acesso pelo collab:
     """   )
     image = Image.open("./imagens/Base de Dados/notebook.png")
--- pages/4_Relatorio.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title('Relatório:')
-# Embed the reveal.js presentation using an iframe
-iframe_code = '<iframe src="https://onedrive.live.com/embed?resid=1F95875ED93EE176%2196167&amp;authkey=!AJgTggXUq0HIwY0&amp;em=2&amp;wdAr=1.7777777777777777" width="1360px" height="768px" frameborder="0">Este é um apresentação do <a target="_blank" href="https://office.com">Microsoft Office</a> incorporado, da plataforma <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>'
-
-# Render the iframe code
-st.markdown(iframe_code, unsafe_allow_html=True)
