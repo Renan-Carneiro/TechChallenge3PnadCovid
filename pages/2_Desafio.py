2ab973c29790ebc5a034edaad6d1e575c161cf1d
Renan Carneiro <113647888+Renan-Carneiro@users.noreply.github.com>
2024-05-13 21:29:39 -0300

    5

--- pages/2_Desafio.py
+++ pages/2_Desafio.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-
+st.title('Desafio')
 st.markdown("""
 # **Introdução**
 
--- pages/3_Banco de Dados.py
+++ pages/3_Banco de Dados.py
@@ -1,46 +1,21 @@
 import streamlit as st
-import pandas as pd
+from PIL import Image
 
-st.title('Tabelas dos Dados de Exportação de Vinhos')
+st.title('Banco de Dados')
 
-url='https://raw.githubusercontent.com/Renan-Carneiro/TechExpVinho/main/ExpVinhoTratado.csv'
-df = pd.read_csv(url, index_col=0)
+st.markdown("""
+# PNAD COVID
+A Pesquisa Nacional por Amostra de Domicílios – COVID-19 ou simplesmente PNAD Covid, não é apenas uma pesquisa, mas uma iniciativa do IBGE (Instituto Brasileiro de Geografia e Estatística) que buscou entender efeitos da pandemia em nosso país.
+Através do PNAD, buscou-se mensurar o impacto da pandemia nas mais diferentes esferas, e, por essa razão, foi necessário um vasto questionário com perguntas envolvendo questões clínicas, socioeconômicas e características da população.
+Sem uma pesquisa dessa magnitude, seria extremamente difícil entender os problemas causados, bem como identificar os mais afetados pela maior crise sanitária do século.
+Através da análise dos dados colhidos, é possível traçar estratégias políticas que poderão não só minimizar o impacto como também evitar futuros cenários como o que vimos durante a pandemia.
 
-#agrupando pela soma de litros e preço
-tabela = df.groupby('País').agg({'Litros Exportados': 'sum', 'Preço Dólar': 'sum'}).reset_index()
-
-#dropando linhas onde ambos valores são 0
-tabela = tabela.loc[(tabela['Preço Dólar'] != 0) | (tabela['Litros Exportados'] != 0)]
-
-# por preço em ordem decrescente
-tabela_preco = tabela.sort_values(by='Preço Dólar', ascending=False).reset_index(drop=True)
-tabela_preco.index += 1
-
-# por litros em ordem decrescente
-tabela_litros = tabela.sort_values(by='Litros Exportados', ascending=False).reset_index(drop=True)
-tabela_litros.index += 1
-
-#função para mostrar em milhões com 3 casa decimais, ou outro formato se for muito pequeno
-def format_magnitude(x):
-    if abs(x) >= 0.1 * 1e6:
-        return "{:.3f} Milhões".format(x / 1e6)
-    elif abs(x) >= 1e3:
-        return "{:.1f} mil".format(x / 1e3)
-    else:
-        return "{:.0f}".format(x)
-
-#modificando valores na tabela de preço
-tabela_preco['Preço Dólar'] = tabela_preco['Preço Dólar'].apply(format_magnitude)
-tabela_preco['Litros Exportados'] = tabela_preco['Litros Exportados'].apply(format_magnitude)
-
-#modificando valores na tabela de litros
-tabela_litros['Preço Dólar'] = tabela_litros['Preço Dólar'].apply(format_magnitude)
-tabela_litros['Litros Exportados'] = tabela_litros['Litros Exportados'].apply(format_magnitude)
-
-# Mostrando com st.dataframe
-st.write('Tabela ordenada por Litros Exportados (País de Origem: Brasil)')
-st.dataframe(tabela_litros)
-
-st.write('Tabela ordenada por Preço (País de Origem: Brasil)')
-st.dataframe(tabela_preco)
-
+## Selecionado as perguntas
+Ao selecionar as perguntas para análise dos dados, consideramos a importância de questões que identificam sintomas, impacto na saúde, acesso a serviços médicos e medidas preventivas durante a pandemia. Além disso, incluímos dados sociodemográficos para uma análise mais detalhada. 
+Iniciamos com a abordagem dos sintomas, destacando a importância de questões que identificam sintomas característicos, como febre, perda de cheiro ou sabor, tosse, entre outros, os quais são cruciais para a identificação de possíveis padrões de manifestações clínicas.
+Além disso, priorizamos perguntas relacionadas ao impacto na saúde e procura por atendimento, que nos permitem avaliar a gravidade da condição de saúde dos indivíduos, a necessidade de recursos médicos e o acesso aos serviços de saúde, evidenciado também pela questão sobre plano de saúde médico.
+É crucial destacar a complexidade da entubação, procedimento delicado que demanda treinamento específico. A escassez de profissionais qualificados para realizar entubações seguras ressalta a necessidade de investimento na formação e preparo de equipes de saúde.
+Considerando o contexto da pandemia do Coronavírus, incorporamos questionamentos sobre as medidas preventivas adotadas, como restrições de contato social e a presença de itens básicos de limpeza e proteção nos domicílios, essenciais para compreender as práticas preventivas e seu impacto na propagação da doença.
+Por fim, a inclusão de dados sociodemográficos, como idade, sexo, etnia, escolaridade, unidade da federação, entre outros, busca fornecer subsídios para uma análise mais detalhada e estratificada dos resultados, considerando as especificidades de diferentes grupos populacionais.
+Dessa forma, a escolha das perguntas para análise reflete a preocupação em obter dados abrangentes e significativos, capazes de contribuir para uma melhor compreensão dos fenômenos em estudo e para o desenvolvimento de estratégias eficazes no âmbito da saúde pública.
+""")
